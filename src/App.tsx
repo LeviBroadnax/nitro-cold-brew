3518aede256f4deda0769575aec62f5d53c79b24
Levi Broadnax <7132948+SystemStack@users.noreply.github.com>
2024-04-26 17:42:29 -0700

    Added Carafe component

--- src/App.tsx
+++ src/App.tsx
@@ -4,22 +4,20 @@ import { OrbitControls, Stage } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import { useWindowSize } from "@uidotdev/usehooks";
 import { useControls } from "leva";
+import { Carafe } from "./components/Carafe";
 
 export default function App() {
   const { width, height } = useWindowSize();
-  const boxProps = useControls({ rotation: [0, 0, 0] });
+  const carafeProps = useControls({
+    rotation: [0, 0, 0],
+    transparent: true,
+    opacity: 0.3,
+  });
   return (
     <Canvas>
       <ambientLight intensity={Math.PI / 2} />
       <Stage>
-        <mesh {...boxProps}>
-          <boxGeometry args={[1, 1, 1]} />
-          <meshToonMaterial
-            color={"hotpink"}
-            transparent={true}
-            opacity={0.1}
-          />
-        </mesh>
+        <Carafe {...carafeProps} />
         <OrbitControls makeDefault />
       </Stage>
     </Canvas>
